c2065e7ed6c89e159826609403a22a5c2925ec46
xMarioOP <marioalejandro424@outlook.com>
2025-01-31 13:38:12 -0500

    Build and deploy project for live demo

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/season-fruits-landing-page/',
   server: {
     proxy: {
       '/api': 'https://www.fruityvice.com',
