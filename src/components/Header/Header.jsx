a9cd1d7c62f6745523c783655111f305008d1e6f
xMarioOP <marioalejandro424@outlook.com>
2025-01-30 20:33:54 -0500

    feat: implement responsive Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Styles.scss"
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Season fruits</h1>
+            <p>THE MOST WONDERFUL FRUITS</p>
+        </header>
+    );
+};
+
+export { Header };
